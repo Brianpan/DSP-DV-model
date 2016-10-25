bad056c9522b0d478377225e1df82b10ac7036d6
Brian Pan <b97705021@ntu.edu.tw>
2016-10-25 22:44:12 +0800

    v1 finish

--- svm_predict.r
+++ svm_predict.r
@@ -1,51 +1,47 @@
 library(e1071)
 
-train_data <- read.csv("/Users/brianpan/Desktop/data/new_output600.csv")
-# train_data <- na.omit(train_data)
-
-# 做資料轉換
-
-
-# count_reg <- glm(Count_plus ~ . - Count - 高危機.死亡 - ACTIONID - 施暴武器說明 - district - lat - lng, data=train_data)
-# step_result <- step(count_reg, direction="backward")
-
-# Count_plus ~ X + TIPVDA + X3 + X7 + X8 + X9 + 家暴因素.子女教養問題 + 
-#     家暴因素.照顧壓力 + 家暴因素.疑似或罹患精神疾病 + 
-#     家暴因素.經濟狀況不佳 + 家暴因素.親屬間相處問題 + 
-#     家暴因素.酗酒 + 暴力型態.精神暴力 + 暴力型態.經濟暴力 + 
-#     暴力型態.肢體暴力 + 受暴持續總月數 + 求助時間差.小時 + 
-#     OCCUPATION + EDUCATION + town
-# svm train
+train_data <- read.csv("/Users/brianpan/Desktop/data/sample.csv")
+train_data <- na.omit(train_data)
 train_data <- transform(train_data, OCCUPATION.無工作=(OCCUPATION=="無工作"))
 train_data <- transform(train_data, OCCUPATION.不詳=(OCCUPATION=="不詳"))
-x <- subset(train_data, select=c(家暴因素.照顧壓力, 家暴因素.子女教養問題, 暴力型態.肢體暴力, 家暴因素.酗酒, 家暴因素.個性.生活習慣不合, 家暴因素.疑似或罹患精神疾病,MAIMED,OCCUPATION.無工作,OCCUPATION.不詳,EDUCATION, TIPVDA, X1.4.5.6))
-y <- train_data$Count_plus
 
-dataset <- subset(train_data, select=c(Count_plus, 家暴因素.照顧壓力, 家暴因素.子女教養問題, 暴力型態.肢體暴力, 家暴因素.酗酒, 家暴因素.個性.生活習慣不合, 家暴因素.疑似或罹患精神疾病,MAIMED,OCCUPATION.無工作,OCCUPATION.不詳,EDUCATION, TIPVDA, X1.4.5.6))
+target_file <- "/Users/brianpan/Desktop/data/train.csv"
+test_data <- read.csv(target_file)
+test_data <- na.omit(test_data)
 
+test_data <- transform(test_data, OCCUPATION.無工作=(OCCUPATION=="無工作"))
+test_data <- transform(test_data, OCCUPATION.不詳=(OCCUPATION=="不詳"))
+
+# model 
+formula_lvq <- factor(Count_plus) ~ 家暴因素.疑似或罹患精神疾病 + 暴力型態.精神暴力 + 求助時間差.小時 + AGE + 家暴因素.照顧壓力 + X8 + 被害人婚姻狀態 + 家暴因素.性生活不協調 + 暴力型態.經濟暴力 + 家暴因素.施用毒品.禁藥或迷幻物品
+formula_stepwise <- factor(Count_plus) ~ X14 + OCCUPATION.不詳 + X10 + X13 + X2 + MAIMED + X1.4.5.6 + 家暴因素.照顧壓力 + 高危機.死亡 + 自殺行為
+formula_rf_5 <- factor(Count_plus) ~ 求助時間差.小時 + AGE + 受暴持續總月數 + 被害人婚姻狀態 + 成人家庭暴力兩造關係
+formula_rf_10 <- factor(Count_plus) ~ 求助時間差.小時 + AGE + 受暴持續總月數 + 被害人婚姻狀態 + 成人家庭暴力兩造關係 + 暴力型態.精神暴力 + EDUCATION + TIPVDA + TIPVDA + 家暴因素.疑似或罹患精神疾病
+
+y <- train_data$group
+
+svm_rdata <- "/Users/brianpan/Desktop/data/svm_features.RData"
+load(svm_rdata)
+
+svm_train_x <- subset(train_data, select=svm_predictors) 
+svm_train_y <- subset(train_data, select=c(group))
+svm_train_data <- cbind(svm_train_x, svm_train_y)
 # tune svm
-svm_tune <- tune(svm, train.x=x, train.y=factor(y), kernel="radial", ranges=list(cost=10^(-1:2), gamma=c(0.5,1,2)))
+svm_tune <- tune(svm, train.x=svm_train_x, train.y=factor(svm_train_y), kernel="radial", ranges=list(cost=10^(-1:2), gamma=c(0.5,1,2)))
 print(svm_tune)
 
-svm_model_after_tune <- svm(factor(Count_plus)~., kernel="radial", cost=10, gamma=2, data=dataset)
-summary(svm_model_after_tune)
+svm_model_after_tune <- svm(formula_stepwise, kernel="radial", cost=as.integer(svm_tune$best.parameters[1]), gamma=as.integer(svm_tune$best.parameters[2]), data=lvq_data)
+# summary(svm_model_after_tune)
 
-pred_tune <- predict(svm_model_after_tune, x)
-table(pred_tune, y)
+pred_tune <- predict(svm_model_after_tune, svm_train_x)
+train_table <- table(pred_tune, y)
 
 # test set
-target_file <- "/Users/brianpan/Desktop/data/train.csv"
-data_list <- read.csv(target_file)
-data_list <- transform(data_list, OCCUPATION.無工作=(OCCUPATION=="無工作"))
-data_list <- transform(data_list, OCCUPATION.不詳=(OCCUPATION=="不詳"))
 
-data_list$Count.處遇中 <- ifelse(data_list$Count.處遇中>2, 3, ifelse(data_list$Count.處遇中==2,2, ifelse(data_list$Count.處遇中==1,1,NA)))
-names(data_list)[2] = "Count_plus"
+testset <- test_data
 
-testdata <- subset(data_list, select=c(Count_plus, 家暴因素.照顧壓力, 家暴因素.子女教養問題, 暴力型態.肢體暴力, 家暴因素.酗酒, 家暴因素.個性.生活習慣不合, 家暴因素.疑似或罹患精神疾病,MAIMED,OCCUPATION.無工作,OCCUPATION.不詳,EDUCATION, TIPVDA, X1.4.5.6))
-testset <-testdata[sample(nrow(testdata), 500),]
+test_y <- testset$group
+test_x <- subset(testset, select= svm_predictors)
 
-test_y <- testset$Count_plus
-test_x <- subset(testset, select=-Count_plus)
 pred_tune <- predict(svm_model_after_tune, test_x)
-table(pred_tune, test_y)
+test_table <- table(pred_tune, y)
--- tree_predict.r
+++ tree_predict.r
@@ -1,65 +1,79 @@
+calc_table <- function(test_data, model){
+	cats_Class <- test_data$group
+	total <- dim(test_data)[1]
+	prob_matrix <- predict(model, test_data)
+	cats_pred_Class <- apply( prob_matrix, 1, function(row) return(colnames(prob_matrix)[which(row == max(row))]) )
+
+	rtable <- table(cats_pred_Class,cats_Class)
+	rrate <- sum(diag(rtable))/total
+	
+	return(rtable)
+}
+
 # CART predictors
 # References: https://c3h3notes.wordpress.com/2010/10/22/r%E4%B8%8A%E7%9A%84cart-package-rpart/
-library("rpart")
+# https://topepo.github.io/caret/recursive-feature-elimination.html#helper-functions
+library(rpart)
+library(randomForest)
+library(mlr)
+library(dplyr)
 
-train_data <- read.csv("/Users/brianpan/Desktop/data/new_output600_noid.csv")
+par(family="LiHei Pro")
+train_data <- read.csv("/Users/brianpan/Desktop/data/sample.csv")
 train_data <- na.omit(train_data)
 train_data <- transform(train_data, OCCUPATION.無工作=(OCCUPATION=="無工作"))
 train_data <- transform(train_data, OCCUPATION.不詳=(OCCUPATION=="不詳"))
+train_data$EDUCATION <- factor(train_data$EDUCATION)
+train_data$MAIMED <- factor(train_data$MAIMED)
+# train_data$被害人婚姻狀態 <- factor(train_data$被害人婚姻狀態)
 
-target_file <- "/Users/brianpan/Desktop/data/train.csv"
+
+
+target_file <- "/Users/brianpan/Desktop/data/train2.csv"
 test_data <- read.csv(target_file)
 test_data <- transform(test_data, OCCUPATION.無工作=(OCCUPATION=="無工作"))
 test_data <- transform(test_data, OCCUPATION.不詳=(OCCUPATION=="不詳"))
-test_data$Count <- ifelse(test_data$Count>2, 3, ifelse(test_data$Count==2,2, ifelse(test_data$Count==1,1,NA)))
-names(test_data)[2] = "Count_plus"
+test_data <- transform(test_data, group=( ifelse(Count > 2, 2, 1) ) ) 
+test_data$EDUCATION <- factor(test_data$EDUCATION)
+test_data$MAIMED <- factor(test_data$MAIMED)
+# test_data$被害人婚姻狀態 <- factor(test_data$被害人婚姻狀態)
+tt <- test_data
+test_data <- subset(test_data, select=-c(district, town))
 
 # model 
-formula_lvq <- factor(Count_plus) ~ 家暴因素.疑似或罹患精神疾病 + 暴力型態.精神暴力 + 求助時間差.小時 + AGE + 家暴因素.照顧壓力 + X8 + 被害人婚姻狀態 + 家暴因素.性生活不協調 + 暴力型態.經濟暴力 + 家暴因素.施用毒品.禁藥或迷幻物品
-formula_stepwise <- factor(Count_plus) ~ X14 + OCCUPATION.不詳 + X10 + X13 + X2 + MAIMED + X1.4.5.6 + 家暴因素.照顧壓力 + 高危機.死亡 + 自殺行為
-formula_rf_5 <- factor(Count_plus) ~ 求助時間差.小時 + AGE + 受暴持續總月數 + 被害人婚姻狀態 + 成人家庭暴力兩造關係
-formula_rf_10 <- factor(Count_plus) ~ 求助時間差.小時 + AGE + 受暴持續總月數 + 被害人婚姻狀態 + 成人家庭暴力兩造關係 + 暴力型態.精神暴力 + EDUCATION + TIPVDA + TIPVDA + 家暴因素.疑似或罹患精神疾病
+rf_rdata <- "/Users/brianpan/Desktop/data/rf_features.RData"
+load(rf_rdata)
+x <- subset(train_data, select=rf_predictors)
+y <- subset(train_data, select=c(group))
+# y <- subset(train_data, select=c(Count))
+rf_train_data <- cbind(x,y)
 
-# train formula
-model_lvq <- rpart(formula_lvq, data=train_data)
-model_stepwise <- rpart(formula_stepwise, data=train_data)
-model_rf_5 <- rpart(formula_rf_5, data=train_data)
-model_rf_10 <- rpart(formula_rf_10, data=train_data)
-
-cats_Class <- test_data$Count_plus
-total <- dim(test_data)[1]
-
-# test 
-lvq_prob_matrix <- predict(model_lvq, test_data)
-
-# 篩選最大預測機率得出來
-lvq_cats_pred_Class <- apply( lvq_prob_matrix, 1, function(row) return(colnames(prob_matrix)[which(row == max(row))]) )
+# model_rf_10 <- rpart(group ~  ., data=rf_train_data)
+rfmodel_rf_10 <- randomForest(group ~ ., data=rf_train_data)
+rfrf_10_t_result <- table(train_data$group, round(predict(rfmodel_rf_10, train_data) ))
 
 
-lvq_table <- table(lvq_cats_pred_Class,cats_Class)
-lvq_rate <- sum(diag(lvq_table))/total
+# model_rf_10 <- rpart(factor(Count) ~  ., data=rf_train_data)
+# rfmodel_rf_10 <- randomForest(factor(Count) ~ ., data=rf_train_data)
+# test
+# rf_10_result <- calc_table(test_data, model_rf_10)
 
-stepwise_prob_matrix <- predict(model_stepwise, test_data)
-stepwise_cats_pred_Class <- apply( stepwise_prob_matrix, 1, function(row) return(colnames(prob_matrix)[which(row == max(row))]) )
+# rfrf_10_result <- table(test_data$group, predict(rfmodel_rf_10, test_data))
+rfrf_10_result <- table(test_data$group, round(predict(rfmodel_rf_10, test_data) ))
 
-stepwise_table <- table(stepwise_cats_pred_Class,cats_Class)
+# 輸出預測的平均 
+district_file <- "/Users/brianpan/Desktop/data/district_rank.csv"
+tt$predict <-predict(rfmodel_rf_10, test_data)
+district_sheet <- tt %>% group_by(district) %>% summarise(avg_predict=mean(predict))
+write.csv(district_sheet, district_file)
 
-stepwise_prob_matrix <- predict(model_stepwise, test_data)
-stepwise_cats_pred_Class <- apply( stepwise_prob_matrix, 1, function(row) return(colnames(prob_matrix)[which(row == max(row))]) )
+# 輸出沒風險被分為高風險的平均
+test_data$predict<-predict(rfmodel_rf_10, test_data)
 
-stepwise_table <- table(stepwise_cats_pred_Class,cats_Class)
-stepwise_rate <- sum(diag(stepwise_table))/total
+to_extract <-test_data[round(test_data$predict)==2 & test_data$group==1,]
+to_extract <- subset(to_extract, select=c(ACTIONID, Count, predict, group, district, town) )
+out<-data.frame(to_extract)
+out_file <- "/Users/brianpan/Desktop/data/rf_numerical.csv"
+write.csv(out, out_file)
 
-
-rf_5_prob_matrix <- predict(model_rf_5, test_data)
-rf_5_cats_pred_Class <- apply( rf_5_prob_matrix, 1, function(row) return(colnames(prob_matrix)[which(row == max(row))]) )
-
-rf_5_table <- table(rf_5_cats_pred_Class,cats_Class)
-rf_5_rate <- sum(diag(rf_5_table))/total
-
-
-rf_10_prob_matrix <- predict(model_rf_10, test_data)
-rf_10_cats_pred_Class <- apply( rf_10_prob_matrix, 1, function(row) return(colnames(prob_matrix)[which(row == max(row))]) )
-
-rf_10_table <- table(rf_10_cats_pred_Class,cats_Class)
-rf_10_rate <- sum(diag(rf_10_table))/total
+reprtree:::plot.getTree(rfmodel_rf_10, k=6)
